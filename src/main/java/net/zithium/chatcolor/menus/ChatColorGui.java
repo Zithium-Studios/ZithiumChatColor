9c1a9c65570485ecb5cf682f53488f6824a9aa6b
Save <itzsaveyt@gmail.com>
2023-09-23 20:57:09 -0500

    Finish up some gui methods.

--- src/main/java/net/zithium/chatcolor/menus/ChatColorGui.java
+++ src/main/java/net/zithium/chatcolor/menus/ChatColorGui.java
@@ -1,6 +1,10 @@
 package net.zithium.chatcolor.menus;
 
+import dev.triumphteam.gui.guis.Gui;
 import net.zithium.chatcolor.ZithiumChatColor;
+import net.zithium.library.utils.Color;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 
 public class ChatColorGui {
 
@@ -9,5 +13,17 @@ public class ChatColorGui {
 
     public ChatColorGui(ZithiumChatColor plugin) {
         this.plugin = plugin;
+    }
+
+
+    public void open(Player player) {
+        FileConfiguration config = plugin.getConfig();
+        Gui gui = Gui.gui()
+                .rows(config.getInt("menu.rows"))
+                .title(Color.componentColor(config.getString("menu.title")))
+                .create();
+
+        gui.setDefaultClickAction(event -> event.setCancelled(true));
+        gui.open(player);
     }
 }
